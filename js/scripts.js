95325d0e8b0213746f7cb90eb5ba15209a7bc3a1
MnHung <MnHung@gmail.com>
2014-12-10 15:41:32 +0800

    add simple signin

--- js/scripts.js
+++ js/scripts.js
@@ -11,5 +11,67 @@ $(document).ready(function(){
 	// start program here while the whole page is ready.
 	$.print("hello contact user");
 	
+	// initial BaasBox
+	// TODO: these should be decide in a config.
+	BaasBox.setEndPoint("http://172.16.252.102:9000");
+	BaasBox.appcode = "1234567890";
 	
+	// initail account
+	var user = BaasBox.getCurrentUser();
+	if (user) {
+		loginSuccess(user);
+	} else {
+		$("#signin-form").show();
+		$("#app").hide();
+	}
+	if (DEBUG) {
+		$.print("BaasBox.getCurrentUser() -> " + BaasBox.getCurrentUser());
+	}
+	//
+	
+	registerSigninEvents();
 });
+
+
+var loginSuccess = function(user) {
+	$("#signin-form").hide();
+	$("#app").show();
+};
+
+function registerSigninEvents() {
+	
+	if (DEBUG) {
+		$("#inputAccount").val("admin");
+		$("#inputPassword").val("admin");
+		setTimeout(function(){
+			$("#signin").click();
+		}, 100);
+	}
+	
+	$("#signin").click(function(e) {
+		$("#signin-error-panel").hide();
+		
+		var user = $("#inputAccount").val(),
+			password = $("#inputPassword").val();
+			
+		if (user == "" || password == "") return;
+		
+		e.preventDefault();
+		
+		BaasBox.login(user, password)
+			.done(function(res) {
+				loginSuccess(user);
+			})
+			.fail(function (err) {
+				$("#signin-error-panel").fadeIn();
+			});
+	});
+	
+	$("#inputPassword").keyup(function(event){
+		if(event.keyCode == 13){
+			$("#signin").click();
+		}
+	});
+	
+}
+
