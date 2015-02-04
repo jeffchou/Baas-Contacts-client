a7743e9a8413e9c99345860bfba81197f53dab76
smallbug917 <smallbug917@gmail.com>
2015-02-04 10:54:17 +0800

    Package BaasBox API - 1.API Settings 2.REST API Access

--- js/extends.baasbox.js
+++ js/extends.baasbox.js
@@ -31,5 +31,53 @@ var BaasBoxEx = {};
 		});
 	};
 
+    BaasBoxEx.fetchCurrentSettings = function() {
+        return $.ajax({
+            method: "GET",
+            url:BaasBox.endPoint + "/admin/configuration/dump.json"
+        });
+    };
+    
+    BaasBoxEx.fectchSectionOfSettings = function(section) {
+        return $.ajax({
+            method: "GET",
+             url: BaasBox.endPoint + "/admin/configuration/" + section
+        });
+    };
+    
+    BaasBoxEx.updateValueInSettings = function(section, key, value) {
+        return $.ajax({
+            method: "PUT",
+            url: BaasBox.endPoint + "/admin/configuration/" + section + "/" +key + "/" + value
+        });
+    };
+    
+    BaasBoxEx.listGroups = function() {
+        return $.ajax({
+            method: "GET",
+            url: BaasBox.endPoint + "/admin/endpoints"
+        });
+    };
+    
+    BaasBoxEx.readSpecificGroup = function(GroupName) {
+        return $.ajax({
+            method: "GET",
+            url: BaasBox.endPoint + "/admin/endpoints/" + GroupName
+        });
+    };
+    
+    BaasBoxEx.enableAnEndpointGroup = function(GroupName) {
+        return $.ajax({
+            method: "PUT",
+            url: BaasBox.endPoint + "/admin/endpoints/" + GroupName + "/enabled"
+        });
+    };
+    
+    BaasBoxEx.disableAnEndpointGroup = function(GroupName) {
+        return $.ajax({
+            method: "DELETE",
+            url: BaasBox.endPoint + "/admin/endpoints/" + GroupName + "/enabled"
+        });
+    };
 	// add more BaasBox extensions
 } (BaasBoxEx, window));
